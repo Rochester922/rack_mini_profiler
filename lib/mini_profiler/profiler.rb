alexhartley/rack-mini-profiler

rack-mini-profiler should use Rails URL helpers when running under rails
When running under Rails, rack-mini-profiler should use the Rails
`#url_for` helper in order to correctly construct the path to the
profiler javascript when in a Rails engine context where SCRIPT_NAME is
inappropriate.

Fixes #25

lib/mini_profiler/profiler.rb
@@ -522,9 +522,13 @@ module Rack
     # * you have disabled auto append behaviour throught :auto_inject => false flag
     # * you do not want script to be automatically appended for the current page. You can also call cancel_auto_inject
     def get_profile_script(env)
-
+      root_url = if env["action_controller.instance"]
+        env["action_controller.instance"].url_for("#{@config.base_url_path}")
+      else
+        "#{env['SCRIPT_NAME']}#{@config.base_url_path}"
+      end
       settings = {
-       :path => "#{env['SCRIPT_NAME']}#{@config.base_url_path}",
+       :path => "#{root_url}",
        :version => MiniProfiler::VERSION,
        :position => @config.position,
        :showTrivial => false,
@@ -564,4 +568,3 @@ module Rack
   end
 
 end
-

Rochester922/rack_mini_profiler

lib/mini_profiler/profiler.rb
@@ -588,7 +588,11 @@ Append the following to your query string:
     # * you have disabled auto append behaviour throught :auto_inject => false flag
     # * you do not want script to be automatically appended for the current page. You can also call cancel_auto_inject
     def get_profile_script(env)
-      path     = "#{env['RACK_MINI_PROFILER_ORIGINAL_SCRIPT_NAME']}#{@config.base_url_path}"
+      path = if env["action_controller.instance"]
+        env["action_controller.instance"].url_for("#{@config.base_url_path}")
+      else
+        "#{env['RACK_MINI_PROFILER_ORIGINAL_SCRIPT_NAME']}#{@config.base_url_path}"
+      end
 
       settings = {
        :path            => path,