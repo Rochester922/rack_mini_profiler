kbrock/rack-mini-profiler

allow environment variable to define sql patching
When users introduce their own patches, or want to work around
patch detection, this allows them to override with
$RACK_MINI_PROFILER_PATCH

setting to false skips sql patches all together.

spec/lib/sql_patches_spec.rb
@@ -1,31 +1,41 @@
 require 'spec_helper'
 
+# this enables activerecord, and allows us to test the auto detection logic
+require 'active_record'
+
 describe SqlPatches do
   # and patched= to some extent
-  describe ".patched?" do
-    it "detects patched" do
-      SqlPatches.patched = true
-
-      expect(SqlPatches).to be_patched
+  describe ".patch_files" do
+    it "uses env variable" do
+      with_patch_env("custom1") do
+        expect(SqlPatches.patch_files).to eq(["custom1"])
+      end
     end
 
-    it "detects unpatched" do
-      SqlPatches.patched = false
+    it "uses supports multiple env variable" do
+      with_patch_env("custom1,custom2") do
+        expect(SqlPatches.patch_files).to eq(["custom1", "custom2"])
+      end
+    end
 
-      expect(SqlPatches).not_to be_patched
+    it "allows env var to turn off" do
+      with_patch_env("false") do
+        expect(SqlPatches.patch_files).to eq([])
+      end
+    end
+
+    it "uses detection of env variable is not defined" do
+      with_patch_env(nil) do
+        expect(SqlPatches.patch_files).to eq(["activerecord"])
+      end
     end
   end
 
-  describe ".unpatched?" do
-    it "detects patched" do
-      SqlPatches.patched = true
-
-      expect(SqlPatches).not_to be_unpatched
-    end
-
-    it "detects unpatched" do
-      SqlPatches.patched = false
-      expect(SqlPatches).to be_unpatched
-    end
+  def with_patch_env(value)
+    old_value = ENV["RACK_MINI_PROFILER_PATCH"]
+    ENV["RACK_MINI_PROFILER_PATCH"] = value
+    yield
+  ensure
+    ENV["RACK_MINI_PROFILER_PATCH"] = old_value
   end
 end

Rochester922/rack_mini_profiler

spec/lib/sql_patches_spec.rb
@@ -1,31 +1,47 @@
 require 'spec_helper'
 
+# this enables activerecord, and allows us to test the auto detection logic
+require 'active_record'
+
 describe SqlPatches do
   # and patched= to some extent
-  describe ".patched?" do
-    it "detects patched" do
-      SqlPatches.patched = true
-
-      expect(SqlPatches).to be_patched
+  describe ".all_patch_files" do
+    it "uses env variable" do
+      with_patch_env("custom1") do
+        expect(SqlPatches.all_patch_files).to eq(["custom1"])
+      end
     end
 
-    it "detects unpatched" do
-      SqlPatches.patched = false
+    it "uses supports multiple env variable" do
+      with_patch_env("custom1,custom2") do
+        expect(SqlPatches.all_patch_files).to eq(["custom1", "custom2"])
+      end
+    end
 
-      expect(SqlPatches).not_to be_patched
+    it "strips whitespace from env variable" do
+      with_patch_env("custom1, custom2") do
+        expect(SqlPatches.all_patch_files).to eq(["custom1", "custom2"])
+      end
+    end
+
+    it "allows env var to turn off" do
+      with_patch_env("false") do
+        expect(SqlPatches.all_patch_files).to eq([])
+      end
+    end
+
+    it "uses detection of env variable is not defined" do
+      with_patch_env(nil) do
+        expect(SqlPatches.all_patch_files).to eq(["activerecord"])
+      end
     end
   end
 
-  describe ".unpatched?" do
-    it "detects patched" do
-      SqlPatches.patched = true
-
-      expect(SqlPatches).not_to be_unpatched
-    end
-
-    it "detects unpatched" do
-      SqlPatches.patched = false
-      expect(SqlPatches).to be_unpatched
-    end
+  def with_patch_env(value)
+    old_value = ENV["RACK_MINI_PROFILER_PATCH"]
+    ENV["RACK_MINI_PROFILER_PATCH"] = value
+    yield
+  ensure
+    ENV["RACK_MINI_PROFILER_PATCH"] = old_value
   end
 end